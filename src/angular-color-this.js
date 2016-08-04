36f08731413d0f08f90447f56e32de8b0ca1ec54
Alexander Hramov <alexander.hramov@gmail.com>
2016-08-04 13:45:50 +0300

    New version

--- src/angular-color-this.js
+++ src/angular-color-this.js
@@ -1,76 +1,98 @@
-angular.module('ngColorThis', [])
-  .factory('Color', function () {
-    function convert(str) {
-      var r, g, b, rOffset, gOffset, bOffset;
-      var rgbObject;
-      var output = '';
-      var salt = -5;
+'use strict';
+/*****************************
+ * Copyright (c) 2016 by Alexander Hramov
+ * Licensed under MIT
+ */
 
-      str = str.toString();
-
-      for (var i = str.length - 1; i >= 0; i--) {
-        var charAt = str.charCodeAt(i).toString().split('');
-        for (var x = charAt.length - 1; x >= 0; x--) {
-          var y = charAt[x] - salt;
-          if (y < 4) y = parseInt(y) + 4;
-          output += Math.pow(y * Math.round(y / 2), 8);
-        }
-      }
-
-      if (output.length % 3 === 0) output = output.split('').reverse().join('');
-
-      rOffset = Math.floor(output.length / 3);
-      gOffset = rOffset * 2;
-      bOffset = output.length - 3;
-
-      r = toVal(output[rOffset], output[rOffset + 1], output[rOffset + 2]);
-      g = toVal(output[gOffset], output[gOffset + 1], output[gOffset + 2]);
-      b = toVal(output[bOffset], output[bOffset + 1], output[bOffset + 2]);
-
-      return {red: r, green: g, blue: b};
-
-    }
-
-    function getContrastColor(red, green, blue) {
-      var y = red * 299 + green * 587 + blue * 114;
-      return y >=128000 ? '#000000' : '#FFFFFF';
-    }
-
-    function toVal(a, b, c) {
-      var together = parseInt(a + b + c);
-      while (together > 255) together = Math.round(together / 5);
-      return together;
-    }
-
-    function rgbToCSS(red, green, blue) {
-      return 'rgb(' + red + ', ' + green + ', ' + blue + ')';
-    }
-
-    return {
-      convert: convert,
-      toVal: toVal,
-      getContrastColor: getContrastColor,
-      rgbToCSS: rgbToCSS
+angular.module('ngColorThis', []).provider('tsColor', function () {
+    var saturation = 0.8;
+    var value = 0.7;
+    this.setSaturation = function (s) {
+        saturation = s;
     };
 
-  })
-  .directive('colorThis', function (Color) {
-    return {
-      restrict: 'A',
-      scope: {
-        color: '='
-      },
-      link: function (scope, element, attrs, fn) {
-        scope.$watch('color', function (newValue, oldValue) {
-          if (newValue) {
-            var rgb = Color.convert(newValue);
-            var css = Color.rgbToCSS(rgb.red, rgb.green, rgb.blue);
-            element.css(attrs.colorThis, css);
-            if (attrs.contrastColor) {
-              element.css(attrs.contrastColor, Color.getContrastColor(rgb.red, rgb.green, rgb.blue));
+    this.setValue = function (v) {
+        value = v;
+    };
+
+    this.$get = function () {
+        function fnv32a(str) {
+            var FNV1_32A_INIT = 0x811c9dc5;
+            var hval = FNV1_32A_INIT;
+            for (var i = 0; i < str.length; ++i) {
+                hval ^= str.charCodeAt(i);
+                hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
             }
-          }
-        }, true);
-      }
+            return hval >>> 0;
+        }
+
+        function hsvToRgb(h, s, v) {
+            if (s === 0) {
+                // achromatic (grey)
+                return '#000';
+            }
+            h /= 60;
+            var i = Math.floor(h);
+            var f = h - i;
+            var p = Math.round(v * (1 - s) * 255);
+            var q = Math.round(v * (1 - s * f) * 255);
+            var t = Math.round(v * (1 - s * ( 1 - f )) * 255);
+            v = Math.round(v * 255);
+            switch (i) {
+                case 0:
+                    return {red: v, green: t, blue: p};
+                case 1:
+                    return {red: q, green: v, blue: p};
+                case 2:
+                    return {red: p, green: v, blue: t};
+                case 3:
+                    return {red: p, green: q, blue: v};
+                case 4:
+                    return {red: t, green: p, blue: v};
+                default:        // case 5:
+                    return {red: v, green: p, blue: q};
+            }
+        }
+
+        function convert(str) {
+            var shortHash = fnv32a(str) % 4096;
+            var hue = (shortHash / 4096) * 360;
+            return hsvToRgb(hue, saturation, value);
+        }
+
+        function getContrastColor(red, green, blue) {
+            var y = red * 299 + green * 587 + blue * 114;
+            return y >= 128000 ? '#000000' : '#FFFFFF';
+        }
+
+        function rgbToCSS(red, green, blue) {
+          return 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+        }
+
+        return {
+            convert: convert,
+            getContrastColor: getContrastColor,
+            rgbToCSS: rgbToCSS
+        };
     };
-  });
+
+}).directive('colorThis', ["tsColor", function (Color) {
+    return {
+        restrict: 'A',
+        scope: {
+            color: '='
+        },
+        link: function (scope, element, attrs) {
+            scope.$watch('color', function (newValue) {
+                if (newValue) {
+                    var rgb = Color.convert(newValue);
+                    var css = Color.rgbToCSS(rgb.red, rgb.green, rgb.blue);
+                    element.css(attrs.colorThis, css);
+                    if (attrs.contrastColor) {
+                        element.css(attrs.contrastColor, Color.getContrastColor(rgb.red, rgb.green, rgb.blue));
+                    }
+                }
+            }, true);
+        }
+    };
+  }]);
